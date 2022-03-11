1e7f7e95ee69825c66c51dc501b6ac9c49474fb8
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-11 23:04:01 +0300

    variable_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,10 @@ int main(void)
 	{
 		putchar(ch);
 		if (ch == 'z')
-		
-			for (ch = 'A'; ch <= 'Z'; ch++)
-				putchar(ch);	
-				break;
+		{
+			for (ch = 'A'; ch <= 'Z'; ch++){
+				putchar(ch);}	
+				break;}
 	}
 	printf("\n");
 	return (0);
