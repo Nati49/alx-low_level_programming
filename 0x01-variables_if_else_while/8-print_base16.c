c5de9f45883eae4120591c4b95914301c357ceb5
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-12 00:30:58 +0300

    variable_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int y;
+	char x;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		putchar(y);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
