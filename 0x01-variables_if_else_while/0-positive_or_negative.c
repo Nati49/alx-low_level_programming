96a9b18ab2dd16d7894e88dd8ecfb21477a7a82c
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-11 16:44:02 +0300

    postive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	if (n > 0)
 	{
 		printf("98 is positive\n");
-	}else if (n < 0)
+	} else if (n < 0)
 	{
 		printf("-98 is negative\n");
-	}else
+	} else
 	{
 		printf("0 is zero\n");
 	}
