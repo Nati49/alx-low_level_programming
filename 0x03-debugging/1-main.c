4ee6fa926e1649036ac93319a643faa66748da1a
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-16 23:38:45 +0300

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,12 +4,15 @@
 * main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");/*":"must be "*/
+
 	i = 0;
+
 	while (i < 10)/*i must increase  to make a limit */
 	{
 		putchar(i);
