f86723638b8f76b52fac26d3d3309562d6fd8bc1
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-17 00:02:54 +0300

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,15 +10,11 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");/*":"must be '"' unnecessary '('*/
-
 	i = 0;
-
 	while (i < 10)/*i must increase to set alimit for the loop*/
 	{
 		putchar(i);
 	}
-
 	printf("Infinite loop avoided! \\o/\n");/*"0" unnecessary input*/
-
 	return (0);
 }
