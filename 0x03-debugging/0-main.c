1638f486ff7e743e2768c23e2e053aa6e13fc537
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-16 22:31:56 +0300

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  *main - Debugging.
@@ -11,6 +11,6 @@ int main(void)
 	int i ;
 
 	i = 0 ;
-		positive_or_negative(i);
+	positive_or_negative(i);
 	return (0);
 }
