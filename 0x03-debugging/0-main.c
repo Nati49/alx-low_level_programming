eee3d29faaaa54d9aa807053db4aebd15b7d93eb
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-16 23:06:00 +0300

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- *main.h - Debugging
+ *main - test function of positive and negative
  *Return: 0
  */
 
 int main(void)i
 {
-	int i ;
+int i ;
+i = 0;
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+positive_or_negative(i);
+
+return (0);
 }
