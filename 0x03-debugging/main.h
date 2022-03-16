4e7cedd1ae6e17d6b146cec7f8446e852613fe60
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-17 00:28:01 +0300

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-#endif
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;	
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
