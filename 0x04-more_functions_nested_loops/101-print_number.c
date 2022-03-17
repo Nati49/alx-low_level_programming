ee3e179fa2eb687bb2eba556f73326f46577dcfc
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-18 01:20:28 +0300

    nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_number
+ * Return;void.
+ */
+void print_number(int x)
+{
+	int a = 98;
+	int b = 402;
+	int c = 1024;
+	int d = 0;
+	int f = -98;
+	
+	for (x = a)
+	{
+		_putchar('98');
+	}
+	for (x = b )
+	{
+		_putchar('402');
+	}
+	for (x = c)
+	{
+		_putchar('1024');
+	}
+	for (x = d)
+	{
+		_putchar('0');
+	}
+	for (x = f)
+	{
+		_putchar('-98');
+	}
+}
