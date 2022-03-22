49f74c200075e24cfc141d5162dc83a2664644b6
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-22 22:57:49 +0300

    pointer_array_string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - is a function that print a string in reverse 
+ * @s: string input
+ * return: string
+ */
+void print_rev(char *s)
+{
+	int j;
+	for (j = 0; s[j] != '\0'; j--)
+	{
+		_putchar(s[j]);
+	}
+	-putchar('\n');
+}
