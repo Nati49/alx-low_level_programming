2a3b7f8c214fc0f5819fadc55ea8721bf2e019d2
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-22 22:28:35 +0300

    pointer_array_string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_puts: is afunction that returns a string
+ *@str:string input
+ *return: string
+ */
+void _puts(char *str)
+{
+	int i:
+	for (i = 0; str[i] !='\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
