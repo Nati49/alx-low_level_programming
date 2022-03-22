04d1b2caaf1730034e92226e45cbf97abef6cac4
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-22 21:45:33 +0300

    pointer_array_string

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	int x;
+	int y;
+
+	x = 98;
+	b = 42;
+	printf("x=%d,b=%d\n", x, y);
+	swap_int(&x, &y);
+	printf("a=%d, b=%d\n",x, y);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *swap_int - afunction that swaps the value of two integrs
+ *x:input 1
+ *y:input 2
+ */
+void swap_int(int x, int y)
+{
+	int n = x;
+	x = y
+	y = n;
+}
