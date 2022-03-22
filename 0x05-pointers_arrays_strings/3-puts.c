8c97f452b06a49c41488cad91a1e7dbb64b13764
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-22 22:34:28 +0300

    pointer_array_string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,14 @@
 
 /**
  *_puts: is afunction that returns a string
- *@str:string input
+ *@str; string input
  *return: string
  */
 void _puts(char *str)
 {
 	int i;
-	for (i = 0; str[i] !='\0'; i++)
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
