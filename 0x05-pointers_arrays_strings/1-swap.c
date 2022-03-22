69f1169e5999bb4041d415e14169088ea962815f
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-03-22 21:59:06 +0300

    pointer_array_string

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 /**
  *swap_int - afunction that swaps the value of two integrs
- *x:input 1
- *y:input 2
+ *@x:input 1
+ *@y:input 2
  */
-void swap_int(int x, int y)
+void swap_int(int *x, int *y)
 {
-	int n = x;
-	x = y
-	y = n;
+	int n = *x;
+	*x = *y
+	*y = n;
 }
