0d58d7eebf6e25bb72805b0854a7a2c5d617ab38
natigirmaoch@gmail.com <natigirmaoch@gmail.com>
2022-04-15 00:25:03 +0300

    function_pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+/**
+  *op_add - return the sum of 2 numbers.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: sum.
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  *op_sub - returns difference of 2 numbers
+  *@a:  integer.
+  *@b: integer.
+  *
+  *Return: difference.
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  *op_mul - returns product of two numbers.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: product.
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  *op_div - returns the quotient of 2 numbers.
+
